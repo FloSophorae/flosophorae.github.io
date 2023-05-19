91d72edaec17a5ddf256479d82488bb2625d7733
FloSophorae <2786398891@qq.com>
2023-05-19 21:07:58 +0800

    index-html

--- wiki/settings.py
+++ wiki/settings.py
@@ -117,7 +117,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static2')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
